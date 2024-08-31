4d186c0dff1eb18720173b563dc225a02697a8cf
bgmiddoshacker <lol.lolyebhilol@gmail.com>
2024-08-31 19:48:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7513431675:AAEhsBWDQ8xF4-ZREM0i2nMk0ZtZ7Hseq-k")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("SmexyAdmin")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1596078576")
 
 
 
